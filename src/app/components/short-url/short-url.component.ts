82f552cf0b2579d87660faca13bc5a0808cad65d
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-08-03 11:33:56 -0500

    Url short & spinner works!

--- src/app/components/short-url/short-url.component.ts
+++ src/app/components/short-url/short-url.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShortUrlService } from 'src/app/services/short-url.service';
 
 @Component({
   selector: 'app-short-url',
@@ -10,18 +11,28 @@ export class ShortUrlComponent implements OnInit {
   nombreUrl: string;
   urlShort: string;
   urlProcesada: boolean;
+  loading: boolean;
 
-  constructor() { 
+  constructor(private _shortUrlService: ShortUrlService) { 
     this.nombreUrl = '';
     this.urlShort = '';
     this.urlProcesada = false;
+    this.loading = false;
   }
 
   ngOnInit(): void {
   }
 
   procesarUrl(){
-    
+    this.urlProcesada = false;
+    this.loading = true;
+
+    this._shortUrlService.getUrlShort(this.nombreUrl).subscribe(data =>{
+      this.loading = false;
+      this.urlShort = data.link
+      this.urlProcesada = true;
+      
+    })
   }
 
 }
--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShortUrlService {
+  url = 'https://api-ssl.bitly.com/v4/shorten';
+  token = 'eeb2faa67c52b727ef73713d78bdf914358f545f';
 
-  constructor() { }
+  constructor( private http: HttpClient) {}
+
+  getUrlShort(nombreUrl: string): Observable<any>{
+    const tokenHeader = new HttpHeaders({Authorization: 'Bearer '+this.token})  
+    
+    const body = {
+      long_url: nombreUrl
+    } 
+    return this.http.post(this.url, body, {headers: tokenHeader});
+
+  }
 }
